9f15bd99b7221d8496ec6d9be3431b9f8cc5cb00
Brian Teahan <Brian.teahan@mycit.ie>
2023-05-13 12:59:23 +0100

    Add function to get user choice and catch errors

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_player_choice():
+    print("Enter coordinates for x axis")
+    player_choice_x = input()
+    print("Enter coordinates for y axis")
+    player_choice_y = input()
+    try:
+        if int(player_choice_x) > 4 or int(player_choice_y) > 4:
+            raise ValueError(
+                f"You need to enter a value between 0 and 4"
+                )
+        if int(player_choice_x) < 0 or int(player_choice_y) < 0:
+            raise ValueError(
+                f"You need to enter a value between 0 and 4"
+                )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again\n")
+get_player_choice()
