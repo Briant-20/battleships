cc7f43390e8ae700d833773e3c812451a3ee0e14
Brian Teahan <Brian.teahan@mycit.ie>
2023-05-14 18:40:25 +0100

    Update get_player_choice to give three parameters

--- run.py
+++ run.py
@@ -1,20 +1,34 @@
 import random
 def get_player_choice():
-    print("Enter coordinates for x axis")
-    player_choice_x = input()
-    print("Enter coordinates for y axis")
-    player_choice_y = input()
-    try:
-        if int(player_choice_x) > 4 or int(player_choice_y) > 4:
-            raise ValueError(
-                f"You need to enter a value between 0 and 4"
+    player_choices = {
+    }
+    i = 0
+    while i < 3:
+        print("Enter coordinates for x axis")
+        player_choice_x = input()
+        print("Enter coordinates for y axis")
+        player_choice_y = input()
+        try:
+            if int(player_choice_x) > 4 or int(player_choice_y) > 4:
+                raise ValueError(
+                    f"You need to enter a value between 0 and 4"
+                    )
+            if int(player_choice_x) < 0 or int(player_choice_y) < 0:
+                raise ValueError(
+                    f"You need to enter a value between 0 and 4"
+                    )
+            if int(player_choice_y) in player_choices and player_choices[int(player_choice_y)] == int(player_choice_x):
+                raise ValueError(
+                    f"You cannot enter the same coordinates twice, try again"
                 )
-        if int(player_choice_x) < 0 or int(player_choice_y) < 0:
-            raise ValueError(
-                f"You need to enter a value between 0 and 4"
-                )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again\n")
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again\n")
+            continue
+        i += 1
+        player_choices[int(player_choice_y)] = int(player_choice_x)
+    print(player_choices)
+    return player_choices
+    
 
 def get_computer_choice():
     computer_choice_x = random.randint(0, 4)
@@ -42,4 +56,4 @@ def player_grid(positions_dictionary):
             print(peg ,end="  ")
             x += 1
         print()
-player_grid({0:0,1:1,4:4})
+player_grid(get_player_choice())
